6535bda9ab22e6d2d03bcbd0a4d872209abec104
m87 <m87blackhole@gmail.com>
2013-04-27 21:44:13 +0200

    c

---
+++ global.mod
@@ -0,0 +1,47 @@
+GFORTRAN module version '9' created from simplex.f95
+MD5:192ab67ba044be75360f68c60d6488b6 -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () () ()
+() () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 'form_tab' 'global' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0 ALLOCATABLE DIMENSION) (CHARACTER 1 0 0 0
+CHARACTER ((CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '2048'))) 0 0 () (1
+0 DEFERRED () ()) 0 () () () 0 0)
+3 'func_tab' 'global' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0 ALLOCATABLE DIMENSION) (REAL 8 0 0 0 REAL ())
+0 0 () (1 0 DEFERRED () ()) 0 () () () 0 0)
+4 'global' 'global' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0 0)
+5 'lim_num' 'global' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 0 () () () 0 0)
+6 'lim_tab' 'global' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0 ALLOCATABLE DIMENSION) (REAL 8 0 0 0 REAL ()) 0 0 () (
+2 0 DEFERRED () () () ()) 0 () () () 0 0)
+7 'mm' 'global' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0) (CHARACTER 1 0 0 0 CHARACTER ((CONSTANT (INTEGER 4 0
+0 0 INTEGER ()) 0 '3'))) 0 0 () () 0 () () () 0 0)
+8 'sig_tab' 'global' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+IMPLICIT-SAVE 0 0 ALLOCATABLE DIMENSION) (CHARACTER 1 0 0 0 CHARACTER (
+(CONSTANT (INTEGER 4 0 0 0 INTEGER ()) 0 '2'))) 0 0 () (1 0 DEFERRED ()
+()) 0 () () () 0 0)
+9 'simplex_table' 'global' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0 ALLOCATABLE DIMENSION) (REAL 8 0 0 0 REAL ())
+0 0 () (2 0 DEFERRED () () () ()) 0 () () () 0 0)
+10 'var_num' 'global' '' 1 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC
+UNKNOWN IMPLICIT-SAVE 0 0) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 0 () ()
+() 0 0)
+)
+
+('form_tab' 0 2 'func_tab' 0 3 'global' 0 4 'lim_num' 0 5 'lim_tab' 0 6
+'mm' 0 7 'sig_tab' 0 8 'simplex_table' 0 9 'var_num' 0 10)
--- simplex.f95
+++ simplex.f95
@@ -1,33 +1,83 @@
-program simplexsolver
+module global
       implicit none
-      !real(kind =8),public,dimension(),allocatable
-      integer(kind = 8), public :: limit_number
+
+      integer(kind =4) :: var_num, lim_num
+      real(kind =8),dimension(:,:),allocatable :: simplex_table
+      character(len =2048), dimension(:),allocatable :: form_tab
+      real(kind=8) , dimension(:),allocatable :: func_tab 
+      real(kind=8), dimension(:,:),allocatable :: lim_tab
+      character(len =3) :: mm
+      character(len=2), dimension(:),allocatable :: sig_tab
+end module global 
+
+program simplexsolver
+use global
+
+      implicit none
+      integer(kind=4) :: i 
+      integer(kind=4) :: j
+      character(len =1) :: sig
+      
+      write(*,*) 'Co robimy: '
+      read(*,*) mm
+      
+
+      write(*,*) 'Liczba ograniczen: '
+      read(*,*) lim_num
       
       
+      
+      allocate(form_tab(0 : lim_num))
+      
+      write(*,*) 'Podaj funkcje '
+      read(*,*) form_tab(0)
+      write(*,*) 'Podaj ograniczenia '
+      read(*,*) (form_tab(i), i=1,lim_num)
+      
+
+      
+      
+      
+!       allocate(simplex_table(0:30,0:30))
 
 
 
 
 
 stop
-      contains
 
-      subroutine getFormula
-      implicit none
+      contains
       
+            subroutine limits
+            use global
+                  implicit none
+                  integer(kind = 4) :: i,j
+                  
+                  
+                  
+                  
+                  
           
-      end subroutine getFormula
+            end subroutine limits
       
-      subroutine getLimit
-      implicit none
-      integer(kind = 8), intent(in) limit_number
-      
-      write(*,*) 'Liczba ograniczen: '
-      read(*,*) limit_number
-      
-      
-      end subroutine getLimit
-      
-      
+!             subroutine getLimit
+!                   implicit none
+!                   integer(kind = 8), intent(in) limit_number
+!       
+!                   write(*,*) 'Liczba ograniczen: '
+!                   read(*,*) limit_number
+!       
+!       
+!             end subroutine getLimit
+!       
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! 0 = 
+! 1 <
+! 2 >
+! 3 <=
+! 4 >=
+! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! 
+
+
 end program simplexsolver
 
